6a7c74836fefe66261e553dc64bfdc3b55f87c19
dy3722 <dy3722@bs.amalnet.k12.il>
2025-09-28 23:41:38 +0300

    end of task

--- app/src/main/java/com/example/exn082/AnswerActivity.java
+++ app/src/main/java/com/example/exn082/AnswerActivity.java
@@ -1,7 +1,11 @@
 package com.example.exn082;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,13 +14,86 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AnswerActivity extends AppCompatActivity {
+    private final int RESULT_OK = Activity.RESULT_OK;
+    private Intent gi;
+    private double a,b,c,inSqr,x,x1,x2;
+    private ImageView iv;
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+        iv = findViewById(R.id.iv);
+        tv = findViewById(R.id.tv);
+
+        gi = getIntent();
+
+        a = gi.getDoubleExtra("a",1);
+        b = gi.getDoubleExtra("b",0);
+        c = gi.getDoubleExtra("c",0);
+
+        inSqr = b*b-4*a*c;
+
+        if (inSqr < 0 && a > 0) // smile no answer
+        {
+            iv.setImageResource(R.drawable.smile_no_ans);
+            tv.setText("No answer");
+            gi.putExtra("no","No answer");
+        }
+        else if (inSqr == 0 && a > 0) // smile one answer
+        {
+            iv.setImageResource(R.drawable.smile_one_ans);
+            x = -b/(2*a);
+            tv.setText("x = " + x);
+        }
+        else if (inSqr > 0 && a > 0) // smile two answer
+        {
+            iv.setImageResource(R.drawable.smile_two_ans);
+            x1 = (-b + Math.sqrt(inSqr)) / (2*a);
+            x2 = (-b - Math.sqrt(inSqr)) / (2*a);
+            tv.setText("x1 = " + x1 + "\n" + "x2 = " + x2);
+        }
+        else if (inSqr < 0 && a < 0) // cry no answer
+        {
+            iv.setImageResource(R.drawable.cry_no_ans);
+            tv.setText("No answer");
+            gi.putExtra("no","No answer");
+        }
+        else if (inSqr == 0 && a < 0) // cry one answer
+        {
+            iv.setImageResource(R.drawable.cry_one_ans);
+            x = -b/(2*a);
+            tv.setText("x = " + x);
+        }
+        else // cry two answer
+        {
+            iv.setImageResource(R.drawable.cry_two_ans);
+            x1 = (-b - Math.sqrt(inSqr)) / (2*a);
+            x2 = (-b + Math.sqrt(inSqr)) / (2*a);
+            tv.setText("x1 = " + x1 + "\n" + "x2 = " + x2);
+        }
     }
 
     public void Back(View view) {
+        if (inSqr < 0)
+        {
+            gi.putExtra("numAns",0);
+        }
+        else if (inSqr == 0)
+        {
+            gi.putExtra("numAns",1);
+            gi.putExtra("x",x);
+        }
+        else
+        {
+            gi.putExtra("numAns",2);
+            gi.putExtra("x1",x1);
+            gi.putExtra("x2",x2);
+        }
+
+        setResult(RESULT_OK,gi);
+        finish();
     }
 }
--- app/src/main/java/com/example/exn082/MainActivity.java
+++ app/src/main/java/com/example/exn082/MainActivity.java
@@ -1,25 +1,117 @@
 package com.example.exn082;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    private final int REQUEST_CODE = 1;
+    private Intent si;
+    private Random rnd;
+    private double a,b,c;
+    private EditText et1,et2,et3;
+    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rnd = new Random();
+
+        et1 = findViewById(R.id.et1);
+        et2 = findViewById(R.id.et2);
+        et3 = findViewById(R.id.et3);
+        tv2 = findViewById(R.id.tv2);
+
+        si = new Intent(this,AnswerActivity.class);
     }
 
     public void rndbtn(View view) {
+        tv2.setText("");
+        a = rnd.nextInt(20)-10;
+        if (a == 0) a++;
+        et1.setText(a + "");
+
+        b = rnd.nextInt(20)-10;
+        et2.setText(b + "");
+
+        c = rnd.nextInt(20)-10;
+        et3.setText(c + "");
     }
 
     public void ansbtn(View view) {
+        if (!et1.getText().toString().equals("") && !et2.getText().toString().equals("") && !et3.getText().toString().equals(""))
+        {
+            if ((!et1.getText().toString().contains(String.valueOf('-')) || (et1.getText().toString().indexOf('-') == 0) && et1.getText().toString().indexOf('-') != et1.getText().toString().length() - 1) && (!et1.getText().toString().contains(String.valueOf('.')) || (et1.getText().toString().indexOf('.') != 0) && et1.getText().toString().indexOf('.') != et1.getText().toString().length() - 1) && (!et2.getText().toString().contains(String.valueOf('-')) || (et2.getText().toString().indexOf('-') == 0) && et2.getText().toString().indexOf('-') != et2.getText().toString().length() - 1) && (!et2.getText().toString().contains(String.valueOf('.')) || (et2.getText().toString().indexOf('.') != 0) && et2.getText().toString().indexOf('.') != et2.getText().toString().length() - 1) && (!et3.getText().toString().contains(String.valueOf('-')) || (et3.getText().toString().indexOf('-') == 0) && et3.getText().toString().indexOf('-') != et3.getText().toString().length() - 1) && (!et3.getText().toString().contains(String.valueOf('.')) || (et3.getText().toString().indexOf('.') != 0) && et3.getText().toString().indexOf('.') != et3.getText().toString().length() - 1)) {
+                if (Double.parseDouble(et1.getText().toString()) != 0)
+                {
+                    tv2.setTextColor(Color.BLACK);
+                    tv2.setText("");
+
+                    a = Double.parseDouble(et1.getText().toString());
+                    b = Double.parseDouble(et2.getText().toString());
+                    c = Double.parseDouble(et3.getText().toString());
+
+                    si.putExtra("a",a);
+                    si.putExtra("b",b);
+                    si.putExtra("c",c);
+                    startActivityForResult(si,REQUEST_CODE);
+                }
+                else
+                {
+                    tv2.setTextColor(Color.RED);
+                    tv2.setText("a - cannot be 0!");
+                }
+            }
+            else
+            {
+                tv2.setTextColor(Color.RED);
+                tv2.setText("Invalid number!");
+            }
+        }
+        else
+        {
+            tv2.setTextColor(Color.RED);
+            tv2.setText("You must enter all the coefficients!");
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int source, int result, @Nullable Intent data_back)
+    {
+        super.onActivityResult(source,result,data_back);
+        if (source == REQUEST_CODE)
+        {
+            if (result == Activity.RESULT_OK)
+            {
+                if (data_back != null)
+                {
+                    switch (data_back.getIntExtra("numAns",0))
+                    {
+                        case 0:
+                            tv2.setText("No answer"); break;
+                        case 1:
+                            tv2.setText("x = " + data_back.getDoubleExtra("x",0)); break;
+                        case 2:
+                            tv2.setText("x1 = " + data_back.getDoubleExtra("x1",0) + "\n" + "x2 = " + data_back.getDoubleExtra("x2",0)); break;
+                    }
+                }
+            }
+        }
     }
 }
